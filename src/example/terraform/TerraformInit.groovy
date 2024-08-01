819e340400fc8dec55df917f7d7a08dea51847b7
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 14:26:26 +0530

    Create TerraformInit.groovy

---
+++ src/example/terraform/TerraformInit.groovy
@@ -0,0 +1,9 @@
+package org.example.terraform
+
+class TerraformInit {
+    static void run() {
+        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            sh 'terraform init'
+        }
+    }
+}
