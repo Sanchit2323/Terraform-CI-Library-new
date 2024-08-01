65856664c251f9ff59ae8f5ac5e92e2b8d929bcd
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 14:26:55 +0530

    Create TerraformFormat.groovy

---
+++ src/example/terraform/TerraformFormat.groovy
@@ -0,0 +1,9 @@
+package org.example.terraform
+
+class TerraformFormat {
+    static void run() {
+        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            sh 'terraform fmt -check -diff > terraform_fmt_report.txt'
+        }
+    }
+}
