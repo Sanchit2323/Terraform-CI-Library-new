6f8069191bfb48c9742a9934da60f572cbc90509
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 17:51:28 +0530

    Update TerraformFormat.groovy

--- src/example/terraform/TerraformFormat.groovy
+++ src/example/terraform/TerraformFormat.groovy
@@ -1,9 +1,15 @@
 package example.terraform
 
 class TerraformFormat {
-    static void run() {
-        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            sh 'terraform fmt -check -diff > terraform_fmt_report.txt'
+    def script
+
+    TerraformFormat(script) {
+        this.script = script
+    }
+
+    void run() {
+        script.catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            script.sh 'terraform fmt -check -diff > terraform_fmt_report.txt'
         }
     }
 }
