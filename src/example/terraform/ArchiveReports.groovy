9b486c6ea987e410d752f773d85450661f497f1c
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 18:47:57 +0530

    Update ArchiveReports.groovy

--- src/example/terraform/ArchiveReports.groovy
+++ src/example/terraform/ArchiveReports.groovy
@@ -1,8 +1,8 @@
 package example.terraform
 
 class ArchiveReports {
-    static void run() {
-        echo 'Archiving Reports'
-        archiveArtifacts artifacts: 'terraform_fmt_report.txt, terraform_validate_report.txt, checkov_report.json, tflint_report.json', allowEmptyArchive: true
+    static void run(script) {
+        script.echo 'Archiving Reports'
+        script.archiveArtifacts artifacts: 'terraform_fmt_report.txt, terraform_validate_report.txt, checkov_report.json, tflint_report.json', allowEmptyArchive: true
     }
 }
