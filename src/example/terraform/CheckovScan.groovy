60a7cde520b25e96a91ec0a8e1d4de96e24dfc32
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 18:29:30 +0530

    Update CheckovScan.groovy

--- src/example/terraform/CheckovScan.groovy
+++ src/example/terraform/CheckovScan.groovy
@@ -1,13 +1,20 @@
 package example.terraform
 
 class CheckovScan {
-    static void run(String checkovPath, String workspace) {
-        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            sh """
+    def script
+
+    CheckovScan(script) {
+        this.script = script
+    }
+
+    void run(String checkovPath, String workspace) {
+        script.catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            script.sh """
                 cd ${workspace}
                 ${checkovPath}/checkov -d . -s --output-file-path checkov_report.json
             """
-            sh 'ls -l'
+            script.sh 'ls -l'
         }
     }
 }
+
