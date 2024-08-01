3192e60c107776a28f9b057b4f8574908540b5dc
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 14:27:30 +0530

    Create TerraformValidate.groovy

---
+++ src/example/terraform/TerraformValidate.groovy
@@ -0,0 +1,9 @@
+package org.example.terraform
+
+class TerraformValidate {
+    static void run() {
+        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            sh 'terraform validate > terraform_validate_report.txt'
+        }
+    }
+}
