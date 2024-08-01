e529146728b32b796e016aef2c57d4220df46e60
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-01 15:45:57 +0530

    Update TerraformInit.groovy

--- src/example/terraform/TerraformInit.groovy
+++ src/example/terraform/TerraformInit.groovy
@@ -3,6 +3,8 @@ package example.terraform
 class TerraformInit {
     static void run(Map params) {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+            // Log the message passed from the pipeline
+            echo params.message ?: "Running Terraform Init"
             sh 'terraform init'
         }
     }
